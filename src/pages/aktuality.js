6890b3b30a84615724749274bf312dca57760b59
Vojtěch Loskot <Loskot@porg.cz>
2020-12-20 20:18:23 +0100

    aktuality change

--- src/pages/aktuality.js
+++ src/pages/aktuality.js
@@ -16,6 +16,7 @@ import lyzTest from '../../assets/testLyzi.png'
 import sezona from '../../assets/snih1.jpg'
 import zahajeniSezony from '../../assets/zahajeniSezony.jpg'
 import informace from '../../assets/informace.jpg'
+import doKonceRoku from '../../../assets/do_konce_roku.jpg';
 
 export default function page(props) {
     let ref = React.useRef();
@@ -34,11 +35,15 @@ export default function page(props) {
 
             </section>
             <section className="section-page-aktuality">
+                <Aktualita image={doKonceRoku} title="⛷ LYŽUJEME KAŽDÝ DEN DO KONCE ROKU 🏂" date="20.12.2020" url="doKonceRoku"
+                    rawText="...&nbsp;s&nbsp;výjimkou Štědrého dne&nbsp;🎄 (24.&nbsp;12.). Od&nbsp;8:30 do 16&nbsp;hodin. V&nbsp;provozu půjčovna, servis&nbsp;🛠 i&nbsp;lyžařská škola (rezervace a&nbsp;objednávky na&nbsp;čísle&nbsp;734&nbsp;824&nbsp;493). Občerstvení&nbsp;🥪🧃 přes&nbsp;okénko."
+                />
                 <Aktualita image={zahajeniSezony} title="ZAČÍNÁME V PÁTEK 🏂" date="15.12. 2020" url="zahajujemeProvoz"
                     rawText="Ski&nbsp;Aréna Karlov otevře sezónu v&nbsp;pátek 18.&nbsp;prosince 2020 v&nbsp;8.30 hodin. K&nbsp;dispozici bude modrá 💙sjezdovka Family u&nbsp;čtyřsedadlové lanovky Myšák 🐭 a&nbsp;jedna ze&nbsp;sjezdovek u&nbsp;čtyřsedadlové lanovky Karlov Ski&nbsp;Express, kde&nbsp;bude i&nbsp;večerní lyžování..." />
                 <Aktualita image={informace} title="Nové vyhlášení o vstupu do ČR" date="17.12 2020" url="informaceVstup"
                     rawText="Od&nbsp;18.&nbsp;prosince 2020 je&nbsp;omezen vstup a&nbsp;pohyb cizinců na&nbsp;území&nbsp;ČR. Podmínkou pro&nbsp;umožnění vstupu je&nbsp;negativní výsledek PCR testu. Dále není možné přicestovat za&nbsp;účelem návštěvy přátel či&nbsp;rekreace."
                 />
+
                 {/* <Aktualita image={sezona} title="Nová sezóna" date="5.12. 2020" url="novaSezona"
                     rawText="Tak co&nbsp;myslíte? Jaká bude? Zahájili jsme zasněžování! Počasí a&nbsp;vláda rozhodne, kdy&nbsp;se uvidíme&nbsp;😊"
                 /> */}
